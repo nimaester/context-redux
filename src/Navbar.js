4e7c8d09544d7b4a3840d49828e6fa4059e2c60d
nimaester <nimaester@gmail.com>
2020-11-03 17:06:32 -0800

    added users

--- src/Navbar.js
+++ src/Navbar.js
@@ -3,7 +3,9 @@ import React from 'react';
 const Navbar = ({github}) => {
   return (
     <div className='navbar bg-primary'>
-      <i className='fab fa-github'/>{github}
+      <h1>
+        <i className='fab fa-github'/> {github}
+      </h1>
     </div>
   );
 };
--- src/Users.js
+++ src/Users.js
@@ -4,13 +4,19 @@ import users from './users.json';
 const Users = () => {
 
   return (
-
     <Fragment>
-    {users.map(user => (
+      {users.map((user) => (
         <div className='card text-center' key={user.login}>
-          <img src={user.avatar_url} alt='' className='round-img' style={{width: '60px'}}></img>
+          <img
+            src={user.avatar_url}
+            alt=''
+            className='round-img'
+            style={{ width: "60px" }}
+          />
           <h3>{user.login}</h3>
-          <a href={user.html_url} className='btn btn-dark brn-sm my-1'>More</a>
+          <a href={user.html_url} className='btn btn-dark brn-sm my-1'>
+            More
+          </a>
         </div>
       ))}
     </Fragment>
