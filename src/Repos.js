05bfae21947a624b239841808056268365f697d7
nimaester <nimaester@gmail.com>
2020-11-06 00:23:35 -0800

    finished github user finder

--- src/Repos.js
+++ src/Repos.js
@@ -2,12 +2,15 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import Repo from './Repo';
 
+
 const Repos = ({repos}) => {
-  return (
-    {repos.map((repo) => {
-      <Repo key={repo.id} repo={repo}/>
-    })}
-  )
+
+  let display = repos.map(repo => (
+    <Repo repo={repo} key={repo.id}/>
+  ))
+
+  return repos.length > 0 ? display : null
+
 };
 
 Repos.propTypes = {
